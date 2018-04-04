cdf84b2d141cd3adf69a9df8128d5a3f9673e93b
HeZero <727138269@qq.com>
2018-04-03 21:00:28 -0700

    project init

---
+++ src/main/java/com/hsp/test/Application.java
@@ -0,0 +1,15 @@
+package com.hsp.test;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setBannerMode(Banner.Mode.CONSOLE);
+        app.run(args);
+    }
+}
